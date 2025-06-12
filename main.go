4201d663a477bae1f5fea064b6206b39dbbfaf71
Bunniesnu <kimin.kwon@snu.ac.kr>
2025-06-13 01:51:31 +0900

    Check data.json existance

--- main.go
+++ main.go
@@ -14,21 +14,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	choice, err := promptChoice("You do not have a data file.\nPlease choose an option:",
-		"Download all files",
-		"Quit and generate a data file",
-	)
-
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error reading input:", err)
-        os.Exit(1)
-    }
-
-	switch choice {
-	case 1:
-		fmt.Println("Downloading all files...")
-	case 2:
-		fmt.Println("Quitting...\nRefer to the documentation: https://github.com/bunniesnu/phoningdb_downloader#readme")
-		os.Exit(0)
+	dataFile := "data.json"
+	if _, err := os.Stat(dataFile); err != nil {
+		choice, err := promptChoice("You do not have a data file.\nPlease choose an option:",
+			"Download all files",
+			"Quit and generate a data file",
+		)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		if choice == 2 {
+			fmt.Println("Quitting...\nRefer to the documentation: https://github.com/bunniesnu/phoningdb_downloader#readme")
+			os.Exit(0)
+		}
 	}
 }
